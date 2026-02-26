5a2b2b36ac8c4003d15f3a8ca222b1a91fc7a8d8
ValentinHirschi <valentin.hirschi@gmail.com>
2026-02-26 16:55:29 +0100

    Make build.rs options for macos optional.

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 use std::path::Path;
 
 fn main() {
-/*
-    if cfg!(target_os = "macos") {
+
+    if cfg!(target_os = "macos") && std::env::var_os("EXTRA_MACOS_LIBS_FOR_GNU_GCC").is_some() {
         let lib_dir = "/opt/local/lib/libgcc";
         let lib_name = "gcc_s.1.1";
         let lib_path = format!("{}/lib{}.dylib", lib_dir, lib_name);
@@ -12,5 +12,5 @@ fn main() {
             println!("cargo:rustc-link-arg=-l{}", lib_name);
         }
     }
-*/
+
 }
